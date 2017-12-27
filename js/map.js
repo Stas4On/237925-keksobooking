01ee62293035ba841aa855b50b1a39a105ca2e16
Stanislav Kuznetsov <stasmgpu@yandex.ru>
2017-12-27 11:36:59 +0400

    Дорабатывает функционал фильтра и добавляет функцию отложенного запуска сортировки

--- js/map.js
+++ js/map.js
@@ -1,8 +1,17 @@
 'use strict';
 
 (function () {
+  var DEBOUNCE_INTERVAL = 500; // ms
+
   var mapPins = document.querySelector('.map__pins');
   var pins = [];
+  var sortForm = document.querySelector('.map__filters');
+  var typeHouse = sortForm.querySelector('select[name = \'housing-type\']');
+  var priceHouse = sortForm.querySelector('select[name = \'housing-price\']');
+  var roomsHouse = sortForm.querySelector('select[name = \'housing-rooms\']');
+  var guestsHouse = sortForm.querySelector('select[name = \'housing-guests\']');
+  var lastTimeout;
+
   // Добавляет пины на карту
   function addMapPins() {
     var fragment = document.createDocumentFragment();
@@ -30,7 +39,6 @@
     var onLoad = function (data) {
       pins = data;
       renderPins(pins);
-      console.log(data);
     };
 
     window.request.download('https://js.dump.academy/keksobooking/data', onLoad, onError);
@@ -53,30 +61,46 @@
 
   // Фильтрует пины созласно значениям в полях формы
   window.updatePins = function () {
-    var form = document.querySelector('.notice__form');
-    var typeHouse = form.querySelector('select[name = \'type\']').value;
-    var priceHouse = form.querySelector('input[name = \'price\']').value;
-    var roomsHouse = form.querySelector('select[name = \'rooms\']').value;
-    var guestsHouse = form.querySelector('select[name = \'capacity\']').value;
 
     var sortPins = pins.filter(function (it) {
-      return it.offer.type === typeHouse;
+      switch (typeHouse.value) {
+        case 'any':
+          return it.offer.type;
+      }
+      return it.offer.type === typeHouse.value;
     }).filter(function (it) {
-      return it.offer.price === priceHouse;
-    })/*.filter(function (it) {
-      return it.offer.rooms === roomsHouse;
+      switch (priceHouse.value) {
+        case 'middle':
+          return it.offer.price >= 10000 && it.offer.price <= 50000;
+        case 'low':
+          return it.offer.price <= 10000;
+        case 'high':
+          return it.offer.price >= 50000;
+      }
+      return it.offer.price;
     }).filter(function (it) {
-      return it.offer.rooms === roomsHouse;
+      switch (roomsHouse.value) {
+        case 'any':
+          return it.offer.rooms;
+      }
+      return it.offer.rooms === +roomsHouse.value;
     }).filter(function (it) {
-      return it.offer.guests === guestsHouse;
-    })*/;
-    console.log(typeHouse);
-    console.log(priceHouse);
-    console.log(roomsHouse);
-    console.log(guestsHouse);
+      switch (guestsHouse.value) {
+        case 'any':
+          return it.offer.guests;
+      }
+      return it.offer.guests === +guestsHouse.value;
+    });
 
-    console.log(sortPins);
     render(sortPins);
+  };
+
+  // Оложенный запуск функции
+  window.debounce = function (fn) {
+    if (lastTimeout) {
+      window.clearTimeout(lastTimeout);
+    }
+    lastTimeout = window.setTimeout(fn, DEBOUNCE_INTERVAL);
   };
 
   // Перетаскивание основного пина
@@ -127,6 +151,7 @@
     });
   }
 
+  // активирует карту и форму, запускает рендер пинов и добавление к ним обработчиков
   function startUsed(evt) {
     evt.currentTarget.removeEventListener('mouseup', startUsed);
     var form = document.querySelector('.notice__form');
@@ -147,10 +172,21 @@
       clickedElement.classList.add('map__pin--active');
     }, false);
     dragMainPin();
+    typeHouse.addEventListener('change', function () {
+      window.debounce(window.updatePins);
+    });
+    priceHouse.addEventListener('change', function () {
+      window.debounce(window.updatePins);
+    });
+    roomsHouse.addEventListener('change', function () {
+      window.debounce(window.updatePins);
+    });
+    guestsHouse.addEventListener('change', function () {
+      window.debounce(window.updatePins);
+    });
   }
 
   // Работа с DOM
-  // активирует карту и форму, запускает рендер пинов и добавление к ним обработчиков
   var status = document.querySelector('.status__download');
   var map = document.querySelector('.map');
   map.querySelector('.map__pin--main').addEventListener('mouseup', startUsed);
